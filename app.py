4dad0c76e9b6eed46123b5bc849dbdccde55897b
brian <brian.p.coons@gmail.com>
2020-06-26 13:19:55 -0600

    added search and averages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_heroku import Heroku
@@ -16,8 +17,8 @@ env.read_env()
 DATABASE_URL = env("DATABASE_URL")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+# app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 
 db = SQLAlchemy(app)
@@ -27,28 +28,32 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(), nullable = False)
-    results = db.relationship('Result', backref='user')
-    alerts = db.relationship('Alert', backref='user')
+    email = db.Column(db.String(), nullable = False)
+    results = db.relationship('Result')
+    alerts = db.relationship('Alert')
 
-    def __init__(self, name):
+    def __init__(self, name, email):
         self.name = name
+        self.email = email
 
 class Alert(db.Model):
     __tablename__ = "alerts"
     id = db.Column(db.Integer, primary_key = True)
-    year_min = db.Column(db.Integer())
-    year_max = db.Column(db.Integer())
-    make = db.Column(db.String())
-    model = db.Column(db.String())
-    price_min = db.Column(db.Integer())
-    price_max = db.Column(db.Integer())
-    miles_min = db.Column(db.Integer())
-    miles_max = db.Column(db.Integer())
+    year_min = db.Column(db.Integer)
+    year_max = db.Column(db.Integer)
+    make = db.Column(db.String)
+    model = db.Column(db.String)
+    price_min = db.Column(db.Integer)
+    price_max = db.Column(db.Integer)
+    miles_min = db.Column(db.Integer)
+    miles_max = db.Column(db.Integer)
+    deviation = db.Column(db.Integer)
     # user_id = db.Column(db.Integer())
     # only partially working
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="alerts")
 
-    def __init__(self, year_min, year_max, make, model, price_min, price_max, miles_min, miles_max, user_id):
+    def __init__(self, year_min, year_max, make, model, price_min, price_max, miles_min, miles_max, deviation, user_id):
         self.year_min = year_min
         self.year_max = year_max
         self.make = make
@@ -57,20 +62,22 @@ class Alert(db.Model):
         self.price_max = price_max
         self.miles_min = miles_min
         self.miles_max = miles_max
+        self.deviation = deviation
         self.user_id = user_id
 
 class Result(db.Model):
     __tablename__ = "results"
     id = db.Column(db.Integer, primary_key = True)
-    year = db.Column(db.Integer())
-    make = db.Column(db.String())
-    model = db.Column(db.String())
-    miles = db.Column(db.Integer())
-    price = db.Column(db.Integer())
+    year = db.Column(db.Integer)
+    make = db.Column(db.String)
+    model = db.Column(db.String)
+    miles = db.Column(db.Integer)
+    price = db.Column(db.Integer)
     link = db.Column(db.String)
     # user_id = db.Column(db.Integer)
     # not working right
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="results")
 
     def __init__(self, year, make, model, miles, price, link, user_id):
         self.year = year
@@ -81,9 +88,17 @@ class Result(db.Model):
         self.link = link
         self.user_id = user_id
 
+class Avg_price(db.Model):
+    __tablename__ = "average_price"
+    id = db.Column(db.Integer, primary_key = True)
+    average_price = db.Column(db.Integer)
+
+    def __init__(self, average_price):
+        self.average_price = average_price
+
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "name")
+        fields = ("id", "name", "email")
 
 class AlertSchema(ma.Schema):
     class Meta:
@@ -91,7 +106,12 @@ class AlertSchema(ma.Schema):
 
 class ResultSchema(ma.Schema):
     class Meta:
-        fields = ("id", "year", "make", "model", "price", "link", "user_id")
+        fields = ("id", "year", "make", "model", "miles", "price", "link", "user_id")
+
+#In case I want to save the averages
+class PriceAverageSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "average_price")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
@@ -102,23 +122,40 @@ alerts_schema = AlertSchema(many=True)
 result_schema = ResultSchema()
 results_schema = ResultSchema(many=True)
 
+average_schema = PriceAverageSchema()
+averages_schema = PriceAverageSchema(many=True)
+
 # CRUD
 
 # GET
+@app.route("/user/<email>", methods=["GET"])
+def get_user(email):
+    found_user = User.query.filter(User.email == email)
+    # all_users = db.session.query(Alert).join(User).filter(User.id == Alert.user_id).all()
+    userResult = user_schema.dump(found_user)
+
+    if userResult:
+        return jsonify(userResult)
+    else:
+        return jsonify("User not found")
+
+#Get all users
 @app.route("/users", methods=["GET"])
 def get_users():
     all_users = User.query.all()
-    userResult = users_schema.dump(all_users)
+    usersResult = users_schema.dump(all_users)
 
-    return jsonify(userResult)
+    return jsonify(all_users)
 
-@app.route("/alerts", methods=["GET"])
-def get_alerts():
-    all_alerts = Alert.query.all()
+#get alerts by user id
+@app.route("/alerts/<id>", methods=["GET"])
+def get_alerts(id):
+    all_alerts = Alert.query.filter(Alert.user_id == id).all()
     alertResult = alerts_schema.dump(all_alerts)
 
     return jsonify(alertResult)
 
+#Get all results
 @app.route("/results", methods=["GET"])
 def get_results():
     all_results = Result.query.all()
@@ -126,13 +163,59 @@ def get_results():
 
     return jsonify(resultResult)
 
-# GET by ID
+#Get results by user id
+@app.route("/results/<id>", methods=["GET"])
+def get_results_by_id(id):
+    all_results = Result.query.filter(Result.user_id == id).all()
+    resultResult = results_schema.dump(all_results)
 
+    return jsonify(resultResult)
+
+#Search
+@app.route("/search/<make>-<model>-<year_min>-<year_max>-<miles_min>-<miles_max>-<price_min>-<price_max>", methods=["GET"])
+def get_search_results(make, model, year_min, year_max, miles_min, miles_max, price_min, price_max):
+    search_results = db.session.query()\
+        .filter(Result.make.like(make))\
+        .filter(Result.model.like(model))\
+        .filter(Result.year >= year_min)\
+        .filter(Result.year <= year_max)\
+        .filter(Result.miles >= miles_min)\
+        .filter(Result.miles <= miles_max)\
+        .filter(Result.price >= price_min)\
+        .filter(Result.price <= price_max).all()
+    searchResult = results_schema.dump(search_results)
+
+    return jsonify(searchResult)
+
+#Get average price 
+@app.route("/results/price/<make>-<model>-<year_min>-<year_max>", methods=["GET"])
+def get_average_price(make, model, year_min, year_max):
+    average_price = db.session.query(func.avg(Result.price).label('average'))\
+        .filter(Result.make.like(make))\
+        .filter(Result.model.like(model))\
+        .filter(Result.year >= year_min)\
+        .filter(Result.year <= year_max).all()
+    
+    return jsonify(average_price)
+
+
+#Get average miles 
+@app.route("/results/miles/<make>-<model>-<year_min>-<year_max>", methods=["GET"])
+def get_average_miles(make, model, year_min, year_max):
+    average_miles = db.session.query(func.avg(Result.miles).label('average')).filter(
+        Result.make.like(make)).filter(
+        Result.model.like(model)).filter(
+        Result.year >= year_min).filter(
+        Result.year <= year_max
+        ).all()
+
+    return jsonify(average_miles)
 
 # POST new user
 @app.route("/user", methods=["POST"])
 def add_user():
     name = request.json["name"]
+    email = request.json["email"]
 
     new_user = User(name)
 
@@ -154,9 +237,10 @@ def add_alert():
     price_max = request.json["price_max"]
     miles_min = request.json["miles_min"]
     miles_max = request.json["miles_max"]
+    deviation = request.json["deviation"]
     user_id = request.json["user_id"]
 
-    new_alert = Alert(year_min, year_max, make, model, price_min, price_max, miles_min, miles_max, user_id)
+    new_alert = Alert(year_min, year_max, make, model, price_min, price_max, miles_min, miles_max, deviation, user_id)
 
     db.session.add(new_alert)
     db.session.commit()
@@ -195,7 +279,7 @@ def add_result():
 #     db.session.commit()
 #     return car.jsonify(alert)
 
-# DELETE 
+# DELETE alert
 @app.route("/alert/<id>", methods=["DELETE"])
 def delete_alert(id):
     alert = Alert.query.get(id)
@@ -204,7 +288,7 @@ def delete_alert(id):
 
     return jsonify("Alert Deleted and stuff")
 
-# DELETE 
+# DELETE result
 @app.route("/result/<id>", methods=["DELETE"])
 def result_resultt(id):
     result = Result.query.get(id)
@@ -213,7 +297,7 @@ def result_resultt(id):
 
     return jsonify("Result Deleted and stuff")
 
-# DELETE 
+# DELETE user
 @app.route("/user/<id>", methods=["DELETE"])
 def delete_user(id):
     user = User.query.get(id)
@@ -221,6 +305,14 @@ def delete_user(id):
     db.session.commit()
 
     return jsonify("User Deleted and stuff")
+
+# @app.route("/user/<id>", methods=["GET"])
+# def find_user(name):
+#     # user = User.query.get(name)
+#     all_users = User.query.all()
+#     userResult = users_schema.dump(user)
+
+#     return jsonify(userResult)
 
 
 if __name__ == "__main__":
