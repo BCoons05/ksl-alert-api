c7e6cfd9fb3422704297419612d99bf8fd92fa3d
brian <brian.p.coons@gmail.com>
2020-07-21 12:17:38 -0600

    getting error code for debug:

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_heroku import Heroku
 from environs import Env
+from elasticsearch import Elasticsearch
 import os
 
 
@@ -219,7 +220,7 @@ def get_search_results(make, model, year_min, year_max, miles_min, miles_max, pr
 #Search All Cars
 @app.route("/search/<make>-<model>-<year_min>-<year_max>-<miles_min>-<miles_max>-<price_min>-<price_max>", methods=["GET"])
 def get_search_cars(make, model, year_min, year_max, miles_min, miles_max, price_min, price_max):
-    search_cars = db.session.query()\
+    search_cars = Car.query()\
         .filter(Car.make.like(make),\
         Car.model.like(model),\
         Car.year >= year_min,\
