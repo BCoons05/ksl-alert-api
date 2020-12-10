0f926190b0bfa913e65810033eef4321d659de18
brian <brian.p.coons@gmail.com>
2020-12-10 15:24:26 -0700

    testing each or statement

--- app.py
+++ app.py
@@ -393,11 +393,11 @@ def check_alerts():
         Alert.price_min <= price,\
         Alert.price_max >= price,\
         or_(Alert.liters == liters, Alert.liters == 'any'),\
-        or_(Alert.cylinders == cylinders, str(Alert.cylinders) == 'any'),\
-        or_(Alert.drive == drive, Alert.drive == 'any'),\
-        or_(Alert.doors == doors, str(Alert.doors) == 'any'),\
-        or_(Alert.fuel == fuel, Alert.fuel == 'any'),\
-        or_(Alert.seller == seller, Alert.seller == 'any')
+        # or_(Alert.cylinders == cylinders, str(Alert.cylinders) == 'any'),\
+        # or_(Alert.drive == drive, Alert.drive == 'any'),\
+        # or_(Alert.doors == doors, str(Alert.doors) == 'any'),\
+        # or_(Alert.fuel == fuel, Alert.fuel == 'any'),\
+        # or_(Alert.seller == seller, Alert.seller == 'any')
         ).all()
 
     searchAlerts = alerts_schema.dump(search_alerts)
