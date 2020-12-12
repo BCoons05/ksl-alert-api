311a96cabf00677b089078f499d12ba856be7ca4
brian <brian.p.coons@gmail.com>
2020-12-12 15:15:41 -0700

    still..

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ users_schema = UserSchema(many=True)
 class Last_Scrape_Schema(ma.Schema):
     class Meta:
         fields = ("vins")
-    vins = ma.Nested(ma.List)
+    vins = ma.List
 
 
 @app.route("/users", methods=["GET"])
