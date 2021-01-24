f14a9e1197b83565ebd64d906ec139da05cd6435
brian <brian.p.coons@gmail.com>
2021-01-24 09:53:14 -0700

    added .json to search cars

--- app.py
+++ app.py
@@ -485,15 +485,15 @@ def check_alerts():
 
     searchAlerts = jsonify(alerts_schema.dump(search_alerts))
 
-    if(searchAlerts):
+    if(len(searchAlerts.json()) > 0):
         new_car = Car(year, make, model, trim, miles, price, link, vin, liters, cylinders, drive, doors, fuel, title, seller)
-        added_car = add_car_from_search_route(new_car)[0]
+        added_car = add_car_from_search_route(new_car).json()[0]
 
         for alert in searchAlerts:
             new_result = Result(added_car.id, alert.user_id, alert.alert_id)
             add_result_from_diff_route(new_result)
 
-            user = get_user_by_id(alert.user_id)[0]
+            user = get_user_by_id(alert.user_id).json()[0]
             # TODO This is where we call the twilio stuff using user.phone
             print('Deal found. Texting {user.phone}')
 
